908b0d3043538f1eab2cd091d8b85f4fed33701b
blessing <kambaleblessing0@gmail.com>
2022-10-27 17:05:14 +0300

    F-out
    fist-out

---
+++ vote.js
@@ -0,0 +1,17 @@
+var yob = 2018
+var curYr = 2022
+
+var age = curYr - yob
+console.log(yob)
+
+if(age > 18){
+    console.log("Your age is" + age)
+}else{
+    console.log("sorry, you are" + age + "your not qualify")
+}
+
+function vote1() {
+    var can1 = Number(document.getElementById("c1").innerHTML)
+    var sp1 = Number(document.getElementById("s1").innerHTML ) 
+}
+sp1.innerHTML = cand1 + 1
